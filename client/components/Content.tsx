dc6258d076a431a516573de6ad091623e96b98d4
Aziz <abdelazizsbaai@gmail.com>
2021-09-16 19:17:17 +0200

    i can load a static image now

--- client/components/Content.tsx
+++ client/components/Content.tsx
@@ -1,7 +1,12 @@
 // eslint-disable-next-line no-use-before-define
 import React = require('react')
+import TYLER from '../img/simple.jpg'
 
 const Content = () => {
-  return <div>Content</div>
+  return (
+    <div>
+      <img src={TYLER} alt="logo" />
+    </div>
+  )
 }
 export default Content
